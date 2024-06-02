9bff5e7299f91430fefa3d3eb5f0967459159ef8
h0ly0ne <32588743+h0ly0ne@users.noreply.github.com>
2024-06-02 20:48:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KON.Liwest.ChannelFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
